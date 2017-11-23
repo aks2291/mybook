7901a11d8bf90a5099c709f69d6cafdc6ca99496
aks2291 <32098426+aks2291@users.noreply.github.com>
2017-11-23 23:37:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/loginapp',{
+mongoose.connect('mongodb://mybookgrp8db:3hQPlq9d6z4S5VZZ82QuhDaS1xab0jN9xGA7sNhYYGZTgEJbtlOrMdMZ5AujbYc1wEXcAPEWtBISwijd7w9w0A==@mybookgrp8db.documents.azure.com:10250/mean?ssl=true&sslverifycertificate=false',{
 	useMongoClient: true
 });
 var db = mongoose.connection;
@@ -80,7 +80,7 @@ app.use('/',routes);
 app.use('/users',users);
 
 //Set port
-app.set('port',3000);
+app.set('port',80);
 
 app.listen(app.get('port'),function(){
 	console.log('Server started on port '+app.get('port'));
